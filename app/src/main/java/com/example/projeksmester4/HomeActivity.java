53a983274aefa0a4181b3b0c8052b89994f48892
refiE41171660 <47969645+refiE41171660@refisembilanbelas19@gmail.com.com>
2019-06-18 08:18:25 +0700

    tambah riwayat pesan

--- app/src/main/java/com/example/projeksmester4/HomeActivity.java
+++ app/src/main/java/com/example/projeksmester4/HomeActivity.java
@@ -6,17 +6,45 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
     private SessionHandler session;
     private CardView transaksiku;
+    ViewFlipper v_flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //view flipper
+        int images[] = {R.drawable.v_mobil,R.drawable.v_mobil1,R.drawable.v_mobil2,R.drawable.v_mobil3,R.drawable.v_mobil4};
+
+        v_flipper = findViewById(R.id.v_flipper);
+
+        for(int image:images){
+            flipperImages(image);
+        }
+    }
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(4000);//4 sec
+        v_flipper.setAutoStart(true);
+
+        //animation
+        v_flipper.setInAnimation(this,android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(this,android.R.anim.slide_out_right);
+
+
+
+        //endv_flipper
+
         transaksiku = (CardView) findViewById(R.id.pinjam);
         //add
         transaksiku.setOnClickListener(this);
@@ -33,6 +61,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+
 
 
 
